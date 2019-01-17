082e93d00fa6126550f51f6f9b9495e225b8323f
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-01-17 17:24:22 +0100

    Add animate class btn--blue on btn navbar

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
-var headerNav = $('.header');
+var $headerNav = $('.header');
+var $btnNavbar = $('.header .btn');
 var jumbotronHeight = $('.jumbotron').height();
 var topPos = '-' + jumbotronHeight;
 
-console.log(headerNav);
-console.log(jumbotronHeight);
+
 $(document).ready(function() {
 
   $(window).scroll(function() {
+    if ($(this).scrollTop() > jumbotronHeight && !$headerNav.hasClass('header--fixed')) {
 
-    if ($(this).scrollTop() > jumbotronHeight && !headerNav.hasClass('header--fixed')) {
-      headerNav.addClass('header--fixed');
-      headerNav.css('top', top);
-      headerNav.animate({
+      $headerNav.addClass('header--fixed');
+      $headerNav.css('top', top);
+      $btnNavbar.removeClass('btn--white').addClass('btn--blue');
+
+      $headerNav.animate({
         'top': '0'
       }, 800);
-    } else if ($(this).scrollTop() < jumbotronHeight && headerNav.hasClass('header--fixed')) {
-      headerNav.animate({
+
+    } else if ($(this).scrollTop() < jumbotronHeight && $headerNav.hasClass('header--fixed')) {
+
+      $headerNav.animate({
         'top': topPos
       }, {
         complete: function() {
           $(this).after(function() {
-            headerNav.removeClass('header--fixed');
+            $btnNavbar.removeClass('btn--blue').addClass('btn--white');
+            $headerNav.removeClass('header--fixed');
           });
         }
       }, 800);
+
     }
   });
+
 });
